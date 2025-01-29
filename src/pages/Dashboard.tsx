58013aacd85c5bc88e43e97af1023ba21b68e1ea
KarolisG <kgaivenis@gmail.com>
2025-01-29 19:44:17 +0200

    chore: init category listing in dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,19 +6,24 @@ export default function Dashboard() {
   useCategories()
   const userCategories = useUserStore((state: UserState) => state.categories)
 
-  console.log(userCategories)
+  if (!user) {
+    return <p>Please log in to access the dashboard.</p>
+  }
 
   return (
     <div>
       <h1>Dashboard</h1>
-      {user ? (
-        <div>
-          <p>Welcome, {user.email}</p>
-          <button onClick={() => console.log('test click')}>Sign Out</button>
-        </div>
-      ) : (
-        <p>Please log in to access the dashboard.</p>
-      )}
+
+      <div>
+        <p>Welcome, {user.email}</p>
+        <h2>Your categories</h2>
+        <ul>
+          {userCategories.map(category => {
+            return <li key={category.id}>{category.name}</li>
+          })}
+        </ul>
+        <button onClick={() => console.log('test click')}>Sign Out</button>
+      </div>
     </div>
   )
 }
