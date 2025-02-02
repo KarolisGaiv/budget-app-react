7fe8f07fbf9066bc24b87b307b19a09335e2a2f3
KarolisG <kgaivenis@gmail.com>
2025-02-02 10:59:37 +0200

    feat: create user method to set expenses, create custom hook to fetch user expenses from db

---
+++ src/hooks/useExpenses.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react'
+import supabase from '@/supabaseClient'
+import { UserState, useUserStore } from '@/stores/user'
+
+export default function useExpenses() {
+  const user = useUserStore((state: UserState) => state.user)
+  const setExpenses = useUserStore((state: UserState) => state.setExpenses)
+
+  useEffect(() => {
+    async function loadExpenses() {
+      if (!user) {
+        setExpenses([])
+        return
+      }
+
+      const { data, error } = await supabase.from('expenses').select('*').eq('user_id', user.id)
+
+      if (error) {
+        setExpenses([])
+        console.error(error)
+        return
+      }
+      setExpenses(data)
+    }
+
+    loadExpenses()
+  }, [user])
+}
